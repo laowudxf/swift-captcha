de1b5c642f3b61fd6fbc0b7b03bec4629653aa75
zhongzhiyuan <zhongzhiyuan5@gmail.com>
2022-05-19 10:43:51 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-captcha
+
+A description of this package.
---
+++ Tests/swift-captchaTests/swift_captchaTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_captcha
+
+final class swift_captchaTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_captcha().text, "Hello, World!")
+    }
+}
